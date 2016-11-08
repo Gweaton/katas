ff68d6dfd347084a74ac79d9a3fb1cb7a08e0e29
George Eaton <george_eaton@hotmail.co.uk>
2016-11-08 10:33:34 +0000

    passes all tests

--- fizzbuzz_kata/spec/fizzbuzz_spec.rb
+++ fizzbuzz_kata/spec/fizzbuzz_spec.rb
@@ -8,5 +8,13 @@ describe 'fizzbuzz' do
   it 'returns "buzz" when passed a multiple of 5' do
     expect(fizzbuzz(10)).to eq "buzz"
   end
+  it 'returns "fizzbuzz" when passed a multiple of 3 & 5' do
+    expect(fizzbuzz(15)).to eq "fizzbuzz"
+  end
+  it 'returns the number when passed any other number' do
+    expect(fizzbuzz(1)).to eq 1
+    expect(fizzbuzz 59).to eq 59
+    expect(fizzbuzz 97).to eq 97
+  end
 
 end
