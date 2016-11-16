0bda0cb073576e40a069aca36ae69acd4e91d0f1
George Eaton <george_eaton@hotmail.co.uk>
2016-11-16 09:21:48 +0000

    works for all single roman digits

--- roman_numerals/spec/roman_numerals_spec.rb
+++ roman_numerals/spec/roman_numerals_spec.rb
@@ -1,8 +1,33 @@
-require 'roman_numerals'
+require "roman_numerals"
 
-describe 'Roman Numerals' do
-  it 'should accept a number and return a string' do
-    expect(roman_numerals(1)).to be_an_instance_of String
+describe "roman_numerals" do
+
+  context "input" do
+    it "should accept a number and return a string" do
+      expect(roman_numerals(1)).to be_an_instance_of(String)
+    end
   end
+
+  context "single roman digits" do
+    it "should return 'I' when passed 1" do
+      expect(roman_numerals(1)).to eq "I"
+    end
+    it "should return 'V' when passed 5" do
+      expect(roman_numerals(5)).to eq "V"
+    end
+    it "should return 'X' when passed 10" do
+      expect(roman_numerals(10)).to eq "X"
+    end
+    it "should return 'L' when passed 50" do
+      expect(roman_numerals(50)).to eq "L"
+    end
+    it "should return 'C' when passed 100" do
+      expect(roman_numerals(100)).to eq "C"
+    end
+    it "should return 'M' when passed 1000" do
+      expect(roman_numerals(1000)).to eq "M"
+    end
+  end
+
 
 end
